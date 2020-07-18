5c6083ee0dc00ba52e55868bcbbbd337a1cc03e9
Yin-Chen Kris Huang <krishuang168@Yin-Chende-MacBook.local>
2020-07-17 21:19:15 -0700

    Admin-specific Welcome Message

--- routes/users.js
+++ routes/users.js
@@ -46,13 +46,25 @@ router.post("/signup", (req, res) => {
 
 router.post("/login", passport.authenticate("local"), (req, res) => {
   const token = authenticate.getToken({ _id: req.user._id });
-  res.statusCode = 200;
-  res.setHeader("Content-Type", "application/json");
-  res.json({
-    success: true,
-    token: token,
-    status: "You are successfully logged in!",
-  });
+
+  if (!req.user.admin) {
+    res.statusCode = 200;
+    res.setHeader("Content-Type", "application/json");
+    res.json({
+      success: true,
+      token: token,
+      status: "You are successfully logged in!",
+    });
+  } else {
+    // A different welcome message for Admin login
+    res.statusCode = 200;
+    res.setHeader("Content-Type", "application/json");
+    res.json({
+      success: true,
+      token: token,
+      status: "Welcome back, Admin!",
+    });
+  }
 });
 
 router.get("/logout", (req, res, next) => {
